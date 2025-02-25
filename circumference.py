2afe7d799d8486cb04633b13bb448463433b0a8d
jovialjanet <janet.do@stu.ocsb.ca>
2025-02-25 19:29:44 +0000

    Added circumference.py and circumference.

---
+++ circumference.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""""
+Created by: Janet Do
+Created on: Feb 2025
+This program calculates the circumference of a circle with a diameter of 7 mm.
+"""
+
+import math
+
+
+def main() -> None:
+    """
+    The main() function calculates the circumference of a 7 mm diameter circle,
+        returns None.
+    """
+    print("If a circle has a diameter of 7 mm: ")
+    print("")
+    print(f"Circumference is {(math.pi * 7):.2f} mm.")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
